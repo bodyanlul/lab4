5ef634e9fe42e204324817a2caf937a89d576e99
bodyanlul <b-volkov@list.ru>
2022-05-13 21:40:23 +0700

    upd

---
+++ src/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <footer>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="footer-content">
+          <div class="logo-home"></div>
+          <div class="footer-text">
+            <div class="social-links">
+              <span class="social otchet"><a href="https://vk.com/doc172927617_638173056?hash=VDa27f4hbgzJpZdCO0nXJZXgOZtIRfyMQKA8m22JXWz&dl=o30CUXjRqXpn4iHbVZpkV9iPMYqx3iq61CJu8buleIs" target="_blank">Отчет</a></span>
+              <span class="social github"><a href="https://github.com/bodyanlul" target="_blank">Github</a></span>
+            </div>
+
+            <p>© 2022 АВТ-913. Все права защищены</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  min-height: 300px;
+  text-align: center;
+  background-color: #111222;
+  padding-top: 120px;
+  font-size: 16px;
+  font-weight: 200;
+  color: #969696;
+  padding-right: 20px;
+  padding-bottom: 20px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
